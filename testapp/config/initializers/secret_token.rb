37ef45558424464039a816bdd145a3ad72879ee9
joofsh <jonathanpagano@gmail.com>
2012-11-05 11:30:15 -0500

    Add testapp to gem

---
+++ testapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '22f7c279170f1769bafd75b5276e5c3de6eb18aa1445da09660ab1ec90c981dca4c7da5f0cff48cf68fbb06b2cee4b057c7e4d8f826755a6f162db1dc68b5b3b'
