7cbe7b0e2278b09bc66b3e62dcfb95ea38b10895
joofsh <jonathanpagano@gmail.com>
2012-11-05 11:19:54 -0500

    Add original coffeescript version to gem

--- vendor/assets/javascripts/geofsh.js
+++ vendor/assets/javascripts/geofsh.js
@@ -1,28 +1,29 @@
-  var geolocError, getPosition, hideAddressAndZip, hideGeoLoc, geolocSuccess;
+var geolocError, getPosition, hideAddressAndZip, hideGeoLoc, geolocSuccess;
 
-  geolocSuccess = function(position) {
-      $('input#latitude').val(position.coords.latitude);
-      return $('input#longitude').val(position.coords.longitude);
-  };
+geolocSuccess = function(position) {
+  $('input#latitude').val(position.coords.latitude);
+  return $('input#longitude').val(position.coords.longitude);
+};
 
-  geolocError = function() {
-    hideGeoLoc();
-    return "Geolocation Unsuccessful";
-  };
+geolocError = function() {
+hideGeoLoc();
+return "Geolocation Unsuccessful";
+};
 
 
-  hideGeoLoc = function() {
-    return $('.geoloc-hide').parent().hide();
-  };
+hideGeoLoc = function() {
+return $('.geoloc-hide').parent().hide();
+};
 
-  getPosition = function() {
-    if (navigator.geolocation) {
-    return navigator.geolocation.getCurrentPosition(geolocSuccess, geolocError);
-    } else {
-    hideGeoLoc();
-    return "Geolocation not supported";
-    }
-  };
+getPosition = function() {
+if (navigator.geolocation) {
+return navigator.geolocation.getCurrentPosition(geolocSuccess, geolocError);
+} else {
+hideGeoLoc();
+return "Geolocation not supported";
+}
+};
+
 
 $(function() {
     return getPosition();
