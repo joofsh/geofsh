baa99dffbfe941fe6bb51087fd33dc068a1faf59
joofsh <jonathanpagano@gmail.com>
2012-11-07 14:24:50 -0500

    Add google maps API to gem

--- testapp/spec/requests/home_spec.rb
+++ testapp/spec/requests/home_spec.rb
@@ -2,10 +2,21 @@ require 'spec_helper'
 @javascript
 
 describe 'Home Requests' do
-  it 'loads geofsh lat/lng data into fields', js: true do
+  before :each do
     visit '/'
-    find_field('latitude').value.should_not be nil
-    find_field('longitude').value.should_not be nil
   end
+
+
+  it 'loads geofsh lat/lng data into fields', js: true do
+    find_field('latitude').value.should_not be_blank
+    find_field('longitude').value.should_not be_blank
+  end
+
+  it 'loads a google map', js: true do
+    page.should have_selector "div#google-map"
+    page.should have_selector "div", text: "Satellite"
+    page.should have_selector "img", src: "https://maps.gstatic.com/mapfiles/google_white.png"
+  end
+
 end
 
--- vendor/assets/javascripts/geofsh.js
+++ vendor/assets/javascripts/geofsh.js
@@ -1,8 +1,32 @@
-var geolocError, getPosition, hideAddressAndZip, hideGeoLoc, geolocSuccess;
+var geolocError, addMarker, getPosition, hideAddressAndZip, hideGeoLoc, geolocSuccess, googleMap, mapOptions, map, marker, geolat, geolng, geoCoords;
 
+googleMap = function(geolat, geolng) {
+  if(document.getElementById("google-map")){
+    geoCoords = new google.maps.LatLng(geolat, geolng);
+    mapOptions = {
+      zoom: 16,
+      center: geoCoords,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
+    addMarker(geoCoords, "You are here!");
+  }
+};
+
+addMarker = function(position, title) {
+  marker = new google.maps.Marker({
+    position: position,
+    map: map,
+    title: title
+    });
+}
 geolocSuccess = function(position) {
-  $('input#latitude').val(position.coords.latitude);
-  return $('input#longitude').val(position.coords.longitude);
+  geolat = position.coords.latitude;
+  geolng = position.coords.longitude;
+  $('input#latitude').val(geolat);
+  $('input#longitude').val(geolng);
+  return googleMap(geolat, geolng);
 };
 
 geolocError = function() {
@@ -16,16 +40,16 @@ return $('.geoloc-hide').parent().hide();
 };
 
 getPosition = function() {
-if (navigator.geolocation) {
-return navigator.geolocation.getCurrentPosition(geolocSuccess, geolocError);
-} else {
-hideGeoLoc();
-return "Geolocation not supported";
-}
+  if (navigator.geolocation) {
+    return navigator.geolocation.getCurrentPosition(geolocSuccess, geolocError);
+  } else {
+    hideGeoLoc();
+    return "Geolocation not supported";
+  }
 };
 
 
 $(function() {
-    return getPosition();
+  return getPosition();
 });
 
