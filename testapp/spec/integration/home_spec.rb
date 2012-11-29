51a6f992fc7b68bcda62fce326405c7680674261
joofsh <jonathanpagano@gmail.com>
2012-11-29 17:14:59 -0500

    Move testapp tests to integration dir

--- testapp/spec/integration/home_spec.rb
+++ testapp/spec/integration/home_spec.rb
@@ -8,11 +8,13 @@ describe 'Home Requests' do
 
 
   it 'loads geofsh lat/lng data into fields', js: true do
+    sleep 1.5
     find_field('latitude').value.should_not be_blank
     find_field('longitude').value.should_not be_blank
   end
 
   it 'loads a google map', js: true do
+    sleep 1.5
     page.should have_selector "div#google-map"
     page.should have_selector "div", text: "Satellite"
     page.should have_selector "img", src: "https://maps.gstatic.com/mapfiles/google_white.png"
